26067abb75291a29d6ca89e27c403fef10763b7d
Kevin Tjin <kevin_tjin@hotmail.com>
2018-09-27 16:37:34 +0200

    First project push

---
+++ PGD B5 Gamejam 3/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject target;
+    public float speed = 5f;
+    public float offSet = 10f;
+   // Vector3 Offset;
+   // public float followSpeed = 1f;
+	void Start () {
+      //  Offset = new Vector3(0,0,-10); //Camera needs an z offset of -10 (or another negative value) in order to see the player
+
+    }
+	
+	void Update () {
+
+        var targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+        // Smoothly rotate towards the target point.
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+
+        Vector3 cameraToTargetPos = transform.position;
+        cameraToTargetPos.z = target.transform.position.z -offSet;
+        transform.position = Vector3.Lerp(transform.position, cameraToTargetPos, speed * Time.deltaTime); 
+    }
+}
